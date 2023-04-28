0dae57f93e502617e7f6e945ad5f2ed17677463e
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-04-28 12:47:28 +0700

    create authentication with firebase config

--- frontend/src/stores/auth.js
+++ frontend/src/stores/auth.js
@@ -1,37 +1,137 @@
 import { defineStore } from 'pinia'
+import {authFirebase,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+  GithubAuthProvider,
+  signInWithPopup
+ } from '@/helpers/authentication/firebaseConfig.js'
+
+
+import axios from "@/helpers/utils/axiosInstance";
 
 export  const useAuthStore = defineStore('auth',{
   state:()=>{
     return{
-      token: null,
-      isAuthenticated: false,
+      token:null,
+      user:null,
+      isAuthenticated:false
     }
   },
   actions:{
-    initialStore(){
-      const token = localStorage.getItem('token')
-      if(token){
-        this.token = token
+    async login(email,password){
+    
+        await signInWithEmailAndPassword (authFirebase,email,password).then(async (userCredential) => {
+          console.log(userCredential)
+          // Signed in
+          
+          this.token = await authFirebase.currentUser.getIdToken()
+           
+          console.log(this.token);
+          const response = await axios.post('/api/users/firebase/auth/',{"token":this.token})
+          console.log(response.data);
+          this.isAuthenticated = true
+          // ...
+        }).catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode);
+          console.log(errorMessage);
+          // ..
+        }
+        );
+       
+       
+        
+       
+    },
+    async register(email,password,confirm_password){
+        try{
+          if(password === confirm_password){
+            await createUserWithEmailAndPassword(authFirebase,email,password).then(async (userCredential)=>{
+              const user = userCredential.user;
+              this.token = await user.getIdToken()
+              console.log(user);
+            })
+           
+            
+          }
+
+        } catch(error) {
+          throw error
+        }
+
+    },
+    async loginGoogle(){
+      const provider = new GoogleAuthProvider();
+      await signInWithPopup(authFirebase,provider).then(async (result) => {
+        const credential =  GoogleAuthProvider.credentialFromResult(result);
+        this.token = await authFirebase.currentUser.getIdToken();
+        console.log(this.token);
+        const response = await axios.post('/api/users/firebase/auth/',{"token":this.token})
+        console.log(response.data);
         this.isAuthenticated = true
-      }else{
-        this.token = null
-        this.isAuthenticated = false
-      }
+        const user = result.user;
+        console.log(user);
+        // ...
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.email;
+        const credential = error.credential;
+        console.log(errorCode);
+        console.log(errorMessage);
+        console.log(email);
+        console.log(credential);
+        // ...
+      });
+    },
+    async  loginFacebook(){
+      const provider = new FacebookAuthProvider();
+      await signInWithPopup(authFirebase,provider).then(async (result) => {
+        const credential =  FacebookAuthProvider.credentialFromResult(result);
+        console.log(credential)
+        this.token = await authFirebase.currentUser.getIdToken();
+        console.log(this.token);
+        const response = await axios.post('/api/users/firebase/auth/',{"token":this.token})
+        console.log(response.data);
+        this.isAuthenticated = true
+        const user = result.user;
+        console.log(user);
+
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.email;
+        const credential = error.credential;
+        console.log(errorCode);
+        console.log(errorMessage);
+        console.log(email);
+        console.log(credential);
+      });
 
 
     },
-    setToken(token){
-      localStorage.setItem('token',token)
-      this.token = token
-      this.isAuthenticated = true
-    },
-    logout(){
-      localStorage.removeItem('token')
-      this.token = null
-      this.isAuthenticated = false
+    async loginGithub(){
+
+
+      const provider = new GithubAuthProvider();
+      await signInWithPopup(authFirebase,provider).then(async (result) => {
+        console.log(result)
+        const credential =  GithubAuthProvider.credentialFromResult(result);
+        
+        this.token = await authFirebase.currentUser.getIdToken();
+        console.log(this.token);
+        const response = await axios.post('/api/users/firebase/auth/',{"token":this.token})
+        console.log(response.data);
+        this.isAuthenticated = true
+        const user = result.user;
+        console.log(user);
+
+
+      })
     }
+  },
 
-
-
-  }
 })
